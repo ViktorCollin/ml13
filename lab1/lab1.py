e6861cc5971f914302425f3cf4bc26d1fec1159b
Viktor Collin <viktor.collin@gmail.com>
2013-09-30 14:29:04 +0200

    till simon

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -10,32 +10,32 @@ def calc_entropy():
   ent1 = dt.entropy(data.monk1)
   ent2 = dt.entropy(data.monk2)
   ent3 = dt.entropy(data.monk3)
-  print "\n---------------\nAssignment 1\n---------------"
+  print "\n------------------------------\nAssignment 1 - Entropy\n------------------------------"
   print "Dataset\tEntropy"
   print "Monk1\t%.6f\nMonk2\t%.6f\nMonk3\t%.6f" % (ent1, ent2, ent3)
 
 def calc_gain():
-  print "\n---------------\nAssignment 2\n---------------"
+  print "\n------------------------------\nAssignment 2 - Average gain\n------------------------------"
   i = 1
   print "Dataset\t  a1\t\t  a2\t\t  a3\t\t  a4\t\t  a5\t\t  a6"
   s = "Monk1\t"
   for attr in data.attributes: 
-  	s = s + "%.6f\t" % (dt.averageGain(data.monk1, attr))
+    s = s + "%.6f\t" % (dt.averageGain(data.monk1, attr))
   print s
   s = "Monk2\t"
   for attr in data.attributes: 
-  	s = s + "%.6f\t" % (dt.averageGain(data.monk2, attr))
+    s = s + "%.6f\t" % (dt.averageGain(data.monk2, attr))
   print s
   s = "Monk3\t"
   for attr in data.attributes: 
-  	s = s + "%.6f\t" % (dt.averageGain(data.monk3, attr))
+    s = s + "%.6f\t" % (dt.averageGain(data.monk3, attr))
   print s
 
 def build_tree():
-  print "\n---------------\nAssignment 3\n---------------"
+  print "\n------------------------------\nAssignment 3 - Error\n------------------------------"
   tree = dt.buildTree(data.monk1, data.attributes)
 #drawtree.drawTree(tree)
-  print "dataset\ttraining\ttest"
+  print "Dataset\tE(train)\tE(test)"
   print "Monk1:\t%.6f\t%.6f" % (1-dt.check(tree, data.monk1), 1-dt.check(tree, data.monk1test))
   tree = dt.buildTree(data.monk2, data.attributes)
   print "Monk2:\t%.6f\t%.6f" % (1-dt.check(tree, data.monk2), 1-dt.check(tree, data.monk2test))
@@ -45,18 +45,23 @@ def build_tree():
 
 def calc_next_level():
   #print "\nAverage gain when a5 is choosen"
+  print "\nA5\t  a1\t\t  a2\t\t  a3\t\t  a4\t\t  a5\t\t  a6"
   s = "A5(" 
   for val in data.attributes[4].values:
     subset = dt.select(data.monk1, data.attributes[4], val)
+    t = "\t"
+    for attr in data.attributes: 
+      t = t + "%.6f\t" % (dt.averageGain(subset, attr))
+    print val , t
     best = dt.bestAttribute(subset, data.attributes)
     s = s + best.name + "("
     #print "best attribute: ", best.name
     for value in best.values:
       #print "choose: ", value, "mostCommon: ", dt.mostCommon(dt.select(subset, best, value))
       if(dt.mostCommon(dt.select(subset, best, value))): 
-		s = s + "+"
+        s = s + "+"
       else:
-	    s = s + "-"
+        s = s + "-"
     s = s + ")"
   s = s + ")"
   print "\nOur tree:\t", s
@@ -64,7 +69,7 @@ def calc_next_level():
   
 
 def prune():
-  print "\n---------------\nAssignment 4\n---------------"
+  print "\n------------------------------\nAssignment 4 - Pruning\n------------------------------"
   print "Dataset\t  0.3\t\t  0.4\t\t  0.5\t\t  0.6\t\t  0.7\t\t  0.8"
   partSizes = [0.3, 0.4, 0.5, 0.6, 0.7, 0.8]
   r = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
@@ -78,13 +83,13 @@ def prune():
       while better:
         better = False
         for subTree in dt.allPruned(bestTree):
-          if dt.check(subTree, training) > bestClass:
+          if dt.check(subTree, test) > bestClass:
             bestTree = subTree
-            bestClass = dt.check(subTree, training)
+            bestClass = dt.check(subTree, test)
             better = True
       r[i] += (1-dt.check(bestTree, data.monk1test))
     i += 1
-  print "Monk1\t%0.6f\t%0.6f\t%0.6f\t%0.6f\t%0.6f\t%0.6f" % (r[0]/100, r[1]/100, r[2]/100, r[3]/100, r[4]/100, r[5]/100)
+  print "Monk1\t%0.6f\t%0.6f\t%0.6f\t%0.6f\t%0.6f\t%0.6f\t" % (r[0]/100, r[1]/100, r[2]/100, r[3]/100, r[4]/100, r[5]/100)
   r = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
   i = 0
   for size in partSizes:  
@@ -102,7 +107,7 @@ def prune():
             better = True
       r[i] += (1-dt.check(bestTree, data.monk3test))
     i += 1
-  print "Monk3\t%0.6f\t%0.6f\t%0.6f\t%0.6f\t%0.6f\t%0.6f" % (r[0]/100, r[1]/100, r[2]/100, r[3]/100, r[4]/100, r[5]/100)
+  print "Monk3\t%0.6f\t%0.6f\t%0.6f\t%0.6f\t%0.6f\t%0.6f\t" % (r[0]/100, r[1]/100, r[2]/100, r[3]/100, r[4]/100, r[5]/100)
     #drawtree.drawTree(bestTree)
 
 def partition(data, fraction):
@@ -112,11 +117,13 @@ def partition(data, fraction):
   return ldata[:breakPoint], ldata[breakPoint:]
 
 def main():
+  print "+++++++++++++++++++++++++++++++++++++++++++++++++++\n\tLab 1 - Machine learning (DD2431)\n\t Viktor Collin <vcollin@kth.se>\n\t Simon Osterman <simost@kth.se>"
   calc_entropy()
   calc_gain()
   calc_next_level()
   build_tree()
   prune()
+  print "+++++++++++++++++++++++++++++++++++++++++++++++++++"
   
 
 
