947522fef9e40ddab5233a5c9d124f95cd6dc463
Simon Österman <simon.oesterman@gmail.com>
2013-10-14 17:01:21 +0200

    lab4

--- lab3/bayesboost/adaboost_discriminant.m
+++ lab3/bayesboost/adaboost_discriminant.m
@@ -1,9 +1,20 @@
 function c = adaboost_discriminant(data, mu, sigma, p, alpha, classes, T)
 [M, ~] = size(data);
-c = zeros(1,M);
-for t = 1:T
-    
-end
+c = zeros(M,1);
 
+for m = 1:M
+    tmp = zeros(2,1);
+    for class = classes
+        for t = 1:T
+            tmp(class+1,1) = tmp(class+1,1) + alpha(1,t)*delta(mu(:,:,t), sigma(:,:,t), p(:,t), [data(m,1:2), class]);
+        end
+    end
+    if tmp(1,1) > tmp(2,1)
+        c(m,1) = 0;
+    else
+        c(m,1) = 1;
+    end
+   
+end
 end
 
--- lab4/lab4.py
+++ lab4/lab4.py
@@ -1,6 +1,6 @@
 from random import randint
 from animate import draw
-gamma = 0.1
+gamma = 0.8
 
 def argmax(f, args):
 	mi = None
@@ -30,52 +30,77 @@ trans = ((1 , 3 , 4 , 12), #0
 		 (15, 13, 10, 2 ), #14
 		 (14, 12, 11, 3 )) #15
 		
-rew = ((0 , 0 , 0 , 0), #0 
-	   (0 , 0 ,-1 ,-1), #1
-	   (0 , 0 ,-1 , 0), #2
-	   (0 , 0 , 0 , 0), #3
-	   (-1,-1 , 0 , 0), #4
-	   (0 ,-1 , 0 ,-1), #5
-	   (0 ,-1 , 0 ,-1), #6
-	   (-1, 1 , 0 , 0), #7
-	   (-1, 0 , 0 , 0), #8
-	   (0 ,-1 , 0 ,-1), #9
-	   (0 ,-1 , 0 ,-1), #10
-	   (-1, 0 , 0 , 0), #11
-	   (0 , 0 , 0 , 0), #12
-	   (0 , 0 ,-1 , 1), #13
-	   (0 , 0 ,-1 , 0), #14
-	   (0 , 0 , 0 , 0)) #15
+rew = ((1 ,-2 , 1 ,-2), #0 
+	   (-1, 1 ,-3 ,-2), #1
+	   (1 , -1 ,-3 , -2), #2
+	   (-1 , -2 , 1 , -2), #3
+	   (-3,-2 , -1 , 1), #4
+	   (1 ,-3 , 1 ,-3), #5
+	   (1 ,-3 , 0 ,-3), #6
+	   (-3, 1 , -1 , 1), #7
+	   (-3, -2 , 1 , -1), #8
+	   (0 ,-3 , 1 ,-3), #9
+	   (1 ,-3 , 1 ,-3), #10
+	   (-3, 1 , 0 , -1), #11
+	   (1 , -2 , -1 , -2), #12
+	   (-1 , 0 ,-3 , 1), #13
+	   (0 , -1 ,-3 , 1), #14
+	   (-1 , -2 , -1 , -2)) #15
 	
+rew = (
+    ( 0,-1, 0,-1), #0 
+	  ( 0, 0,-5,-9), #1
+	  ( 0, 0,-5,-1), #2
+	  ( 0,-1, 0,-1), #3
+	  (-5,-9, 0, 0), #4
+	  ( 0,-5, 0,-5), #5
+	  ( 0,-5, 0,-5), #6
+	  (-5, 1, 0, 0), #7
+	  (-5,-1, 0, 0), #8
+	  ( 0,-5, 0,-5), #9
+	  ( 0,-5, 0,-5), #10
+	  (-5,-1, 0, 0), #11
+	  ( 0,-1, 0,-1), #12
+	  ( 0, 0,-5, 1), #13
+	  ( 0, 0,-5,-1), #14
+	  ( 0,-1, 0,-1) #15
+     )
+
 def policyIteration():
-	policy = [None for s in trans]
-	value = [0 for s in trans]
-	
-	for p in range(100):
-		for s in range(len(policy)):
-			policy[s] = argmax(
-			lambda(a):
-				rew[s][a] + gamma * value[trans[s][a]],
-			range(4))
-		for s in range(len(value)):
-			a = policy[s]
-			value[s] = rew[s][a] + gamma * value[trans[s][a]]
-			
-		
-	state = randint(0,len(trans)-1)
-	sequence = [state]
-	for i in range(16):
-		state = policy[state]
-		sequence.append(state)
-	draw(sequence)
-	
-	
+  policy = [None for s in trans]
+  value = [0 for s in trans]
+  
+  for p in range(100):
+    for s in range(len(policy)):
+      policy[s] = argmax(
+      lambda(a):
+        rew[s][a] + gamma * value[trans[s][a]],
+      range(4))
+    for s in range(len(value)):
+      a = policy[s]
+      value[s] = rew[s][a] + gamma * value[trans[s][a]]
+  print_dot(policy)	
+  state = randint(0,len(trans)-1)
+  sequence = [state]
+  for i in range(16):
+    state = trans[state][policy[state]]
+    sequence.append(state)
+  draw(sequence)
+
+def print_dot(policy):
+  print "digraph{"
+  for i in range(len(policy)):
+    print i, "[label=\"\", image=\"step%d.png\"];" % (i+1)
+  for i in range(len(policy)):
+    print i, " -> ", trans[i][policy[i]], ";"
+  print "}"
+
 
 
 def main():
-  print "+++++++++++++++++++++++++++++++++++++++++++++++++++\n\tLab 4 - Machine learning (DD2431)\n\t Viktor Collin <vcollin@kth.se>\n\t Simon Osterman <simost@kth.se>"
+# print "+++++++++++++++++++++++++++++++++++++++++++++++++++\n\tLab 4 - Machine learning (DD2431)\n\t Viktor Collin <vcollin@kth.se>\n\t Simon Osterman <simost@kth.se>"
   policyIteration();
-  print "+++++++++++++++++++++++++++++++++++++++++++++++++++"
+# print "+++++++++++++++++++++++++++++++++++++++++++++++++++"
   
 
 
